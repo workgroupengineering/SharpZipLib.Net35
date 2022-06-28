5aedc078a881ce2c41878ff1b8af7aed91692c85
Giuseppe Lippolis <workgroupengineering@outlook.com>
2022-06-28 10:36:33 +0200

    fix: Replace MinimumAsyncBridge with Theraot.Core

--- src/ICSharpCode.SharpZipLib/Compatibility/StreamExtensions.cs
+++ src/ICSharpCode.SharpZipLib/Compatibility/StreamExtensions.cs
@@ -23,30 +23,6 @@ namespace System.IO
 					base.Dispose(disposing);
 			}
 		}
-
-		public static void CopyTo(this Stream fromStream, Stream toStream)
-
-		{
-
-			if (fromStream == null)
-
-				throw new ArgumentNullException("fromStream");
-
-			if (toStream == null)
-
-				throw new ArgumentNullException("toStream");
-
-
-
-			var bytes = new byte[8092];
-
-			int dataRead;
-
-			while ((dataRead = fromStream.Read(bytes, 0, bytes.Length)) > 0)
-
-				toStream.Write(bytes, 0, dataRead);
-
-		}
 #endif
 		public static StreamWriter GetWriter(this Stream stream, Text.Encoding encoding, int bufferSize, bool leaveOpen)
 		{
@@ -79,67 +55,6 @@ namespace System.IO
 							tcs.SetCanceled();
 						}
 					});
-			return tcs.Task;
-		}
-
-		public static Task<int> ReadAsync(this Stream stream, byte[] buffer, int offset, int count)
-		{
-			var tcs = new TaskCompletionSource<int>();
-
-			stream.BeginRead(buffer, offset, count, asr =>
-				   {
-					   try
-					   {
-						   var result = stream.EndRead(asr);
-						   tcs.SetResult(result);
-					   }
-					   catch (Exception ex)
-					   {
-
-						   tcs.SetException(ex);
-					   }
-				   }, null);
-
-			return tcs.Task;
-		}
-
-		public static Task WriteAsync(this Stream stream, byte[] buffer, int offset, int count)
-		{
-			var tcs = new TaskCompletionSource<bool>();
-			stream.BeginWrite(buffer, offset, count, asr =>
-				   {
-					   try
-					   {
-						   stream.EndWrite(asr);
-						   tcs.SetResult(true);
-					   }
-					   catch (Exception ex)
-					   {
-						   tcs.SetException(ex);
-					   }
-				   }, null);
-			return tcs.Task;
-		}
-
-		public static Task FlushAsync(this Stream stream)
-		{
-			var tcs = new TaskCompletionSource<bool>();
-			Task.Run(() => stream.Flush())
-				.ContinueWith(t =>
-				{
-					if (t.IsCompleted)
-					{
-						tcs.SetResult(true);
-					}
-					else if (t.IsFaulted)
-					{
-						tcs.SetException(t.Exception);
-					}
-					else
-					{
-						tcs.SetCanceled();
-					}
-				});
 			return tcs.Task;
 		}
 #endif
